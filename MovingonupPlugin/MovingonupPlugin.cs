d60432b51e09b9fd67a8162d3f91be3afa74b264
Paul <paul@e-swoosh.co.uk>
2020-12-28 10:19:51 +0000

    Moving on up arriving at stable

---
+++ MovingonupPlugin/MovingonupPlugin.cs
@@ -0,0 +1,70 @@
+﻿using BepInEx;
+using RoR2;
+using System.Reflection;
+using static R2API.SoundAPI;
+
+namespace MovingonupPlugin
+{
+     [BepInDependency("com.bepis.r2api")]
+
+    [BepInPlugin(
+        "com.TakesTheBiscuit.MovingOnUP",
+        "MovingOnUP",
+        "0.3.0")]
+        
+   public class MovingonupPlugin : BaseUnityPlugin
+    {
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            using (var bankStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ExamplePlugin.bank.bnk"))
+            {
+                var bytes = new byte[bankStream.Length];
+                bankStream.Read(bytes, 0, bytes.Length);
+                SoundBanks.Add(bytes);
+            }
+
+            using (var bankStream2 = Assembly.GetExecutingAssembly().GetManifestResourceStream("ExamplePlugin.bank.bnk"))
+            {
+                var bytes2 = new byte[bankStream2.Length];
+                bankStream2.Read(bytes2, 0, bytes2.Length);
+                SoundBanks.Add(bytes2);
+            }
+
+            On.RoR2.GlobalEventManager.OnTeamLevelUp += (orig, self) =>
+            {
+                orig(self);
+
+                // don't spoil the pod dropping in or enemy xp upgrades
+                if (self  == TeamIndex.Player)
+                {
+                    ulong currentExp = TeamManager.instance.GetTeamExperience(self);
+                    if (currentExp < 1)
+                    {
+                        // not this time
+                    }
+                    else
+                    {
+                        // only play AFTER tier1
+                        Chat.AddMessage("Moving on up!");
+                        Util.PlaySound("PlayUp", base.gameObject);
+                    }
+                }
+            };
+            
+            On.RoR2.TeleporterInteraction.OnInteractionBegin += (orig, self, interactor) => {
+                orig(self, interactor);
+                if (TeleporterInteraction.instance.isCharged)
+                {
+                    Chat.AddMessage("Time to break free, nothin can stop me");
+                    Util.PlaySound("PlayUp", base.gameObject);
+                }
+            };
+        }
+
+
+        public void Update()
+        {
+        }
+    }
+}
